a67ebc2fab8eb6c602163723459bc11116198d8b
WataruSuzuki <wataru0406@gmail.com>
2016-05-04 22:31:01 +0900

    Update platform windows

--- platforms/windows/www/js/sampleCodeForStudy.js
+++
@@ -1,9 +0,0 @@
-﻿function dispAlert(){
-    window.alert('アラートの表示');
-}
-
-function loadExternalWebsite(){
-    var stupidExample = '<?xml version="1.0" encoding="utf-8"?><aTag>something</aTag>';
-    window.open('data:Application/octet-stream,' + encodeURIComponent(stupidExample));
-    //window.open('https://watarusuzuki.github.io/apn-profiles/resources/nodata.mobileconfig');
-}
