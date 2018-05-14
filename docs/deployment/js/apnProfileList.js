df5dd188a87ee19f57ae7b6b64a71902ded46628
WataruSuzuki <wataru0406@gmail.com>
2018-05-14 22:10:00 +0900

    Added docs from gh-pages

---
+++ docs/deployment/js/apnProfileList.js
@@ -0,0 +1,33 @@
+"use strict";
+
+angular.module('app')
+.controller('ApnProfileListController', function($scope, watarusuzukigithubio) {
+    var page = onsnavi.topPage;
+    var countryName = page.pushedOptions.items.name;
+
+    watarusuzukigithubio.getProfileInfo(countryName + ".json").then(function(data){
+        $scope.jsonData = data;
+    });
+
+    $scope.openConfigProfile = function(index) {
+        var profileItem = $scope.jsonData.items[index];
+        var ref = window.open(profileItem.profile_url, '_blank', 'location=yes');
+    };
+});
+
+angular.module('app')
+.factory('watarusuzukigithubio', function($http) {
+    return {
+		    getProfileInfo: function(fileName){
+                var resultsApnBookmarks = $http.get('https://watarusuzuki.github.io/APNBookmarkPage/jsons/' + fileName)
+	            .then(function(response){
+	                return response.data;
+				},
+				function(error){
+					alert("Error Details : " + error.status);
+				}
+			);
+            return resultsApnBookmarks;
+        }
+    }
+});
